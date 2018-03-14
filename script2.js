bd20298829ec40eecd49ae4442d5afd876c5e4bb
hghleow <gretaleow@gmail.com>
2018-03-14 18:24:09 +0800

    commit msg

--- script2.js
+++ script2.js
@@ -1,31 +1,168 @@
+//Scenario 1
 
-var saySomething1 = function() {
+//Question 1
+var s1o1 = function() {
 	alert("Oops! Try harder.");
 };
 
-document.getElementById("opt1")
-	.addEventListener("click", saySomething1);
+document.getElementById("opt1-1-1")
+	.addEventListener("click", s1o1);
 
-var saySomething2 = function() {
+
+var s1o2 = function() {
 	alert("Oops! Try harder.");
 };
 
+document.getElementById("opt1-1-2")
+	.addEventListener("click", s1o2);
 
-document.getElementById("opt2")
-	.addEventListener("click", saySomething2);
 
-var saySomething3 = function() {
+var s1o3 = function() {
 	alert("Oops! Try harder.");
 };
 
+document.getElementById("opt1-1-3")
+	.addEventListener("click", s1o3);
 
-document.getElementById("opt3")
-	.addEventListener("click", saySomething3);
 
-var saySomething4 = function() {
+var s1o4 = function() {
 	alert("Correct!");
 };
 
+document.getElementById("opt1-1-4")
+	.addEventListener("click", s1o4);
 
-document.getElementById("opt4")
-	.addEventListener("click", saySomething4);
+//Question 2
+var s1o1 = function() {
+	alert("Well done!");
+};
+
+document.getElementById("opt1-2-1")
+	.addEventListener("click", s1o1);
+
+
+var s1o2 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt1-2-2")
+	.addEventListener("click", s1o2);
+
+//Scenario 2
+
+//Question 1
+var s1o1 = function() {
+	alert("Good job!");
+};
+
+document.getElementById("opt2-1-1")
+	.addEventListener("click", s1o1);
+
+
+var s1o2 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt2-1-2")
+	.addEventListener("click", s1o2);
+
+
+var s1o3 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt2-1-3")
+	.addEventListener("click", s1o3);
+
+
+var s1o4 = function() {
+	alert("Oops! Try again.");
+};
+
+document.getElementById("opt2-1-4")
+	.addEventListener("click", s1o4);
+
+//Scenario 3
+
+//Question 1
+
+var s1o1 = function() {
+	alert("Fantastic!");
+};
+
+document.getElementById("opt3-1-1")
+	.addEventListener("click", s1o1);
+
+
+var s1o2 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-1-2")
+	.addEventListener("click", s1o2);
+
+//Question 2
+
+var s1o1 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-2-1")
+	.addEventListener("click", s1o1);
+
+
+var s1o2 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-2-2")
+	.addEventListener("click", s1o2);
+
+
+var s1o3 = function() {
+	alert("Correct!");
+};
+
+document.getElementById("opt3-2-3")
+	.addEventListener("click", s1o3);
+
+
+var s1o4 = function() {
+	alert("Incorrect!");
+};
+
+document.getElementById("opt3-2-4")
+	.addEventListener("click", s1o4);
+
+//Question 3
+
+var s1o1 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-3-1")
+	.addEventListener("click", s1o1);
+
+
+var s1o2 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-3-2")
+	.addEventListener("click", s1o2);
+
+
+var s1o3 = function() {
+	alert("Correct!");
+};
+
+document.getElementById("opt3-3-3")
+	.addEventListener("click", s1o3);
+
+
+var s1o4 = function() {
+	alert("Oops! Try harder.");
+};
+
+document.getElementById("opt3-3-4")
+	.addEventListener("click", s1o4);
